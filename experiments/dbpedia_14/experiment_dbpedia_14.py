062db3bd857e7b0c35ff6ab98ff20d619c2655be
Tomoya Kanda <tomoya.kanda.2022b@mlab.info>
2023-12-14 00:24:02 +0900

    wip

--- experiments/dbpedia_14/experiment_dbpedia_14.py
+++ experiments/dbpedia_14/experiment_dbpedia_14.py
@@ -1,9 +1,12 @@
+import logging
+
 import argparse
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, AIMessage, HumanMessage
 from datasets import load_dataset
 from dotenv import load_dotenv
 
+from ask_llms_examples import ask_positive_and_negative_for_class
 
 load_dotenv()
 
@@ -14,13 +17,30 @@ parser.add_argument("--n_sample_from", default=5, type=int, help="Number of samp
 parser.add_argument("--n_sample_to", default=5, type=int, help="Number of samples to")
 parser.add_argument("--n_sample_step", default=5, type=int, help="Number of samples step")
 parser.add_argument("--model", default="gpt-3.5-turbo", type=str, help="Model name")
+parser.add_argument("--logging", default=False, type=bool, help="Logging to stdout")
 
 args = parser.parse_args()
 
 chat = ChatOpenAI(model=args.model)
 messages = [
-    SystemMessage(content="You should answer with the literal of list of python. For example, ['example1', 'example\\\'2', 'example3']"),
+    SystemMessage(content='''
+You should answer with the literal of list of python. For example, ["example1", "example's 2", "3 examples"]
+'''),
 ]
 
 ds_wiki = load_dataset("dbpedia_14", split="train")
 classlabel_list = ds_wiki.features["label"].names
+
+
+n_trials = args.n_trials
+n_sample_range = range(args.n_sample_from, args.n_sample_to + 1, args.n_sample_step)
+for n_sample in n_sample_range:
+    if args.logging:
+        logging.info(f"sample number: {n_sample}")
+    for trial_iter in n_trials:
+        if args.logging:
+            logging.info(f"trial: {trial_iter}/{n_trials}")
+        for class_idx, label in enumerate(classlabel_list):
+            result = ask_positive_and_negative_for_class(chat, ds_wiki, classlabel_list, n_sample, n_trials)
+            if args.logging:
+                logging.info(result)
